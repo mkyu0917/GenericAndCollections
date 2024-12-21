a2c648f4bc820e4729dec6d76f2d2a4b1f28f4d7
KimMinKyu <mkyu0917@gmail.com>
2024-12-21 16:14:40 +0900

    Generic - 제네릭 기본1 문제풀이

---
+++ src/generic/test/ex2/Pair.java
@@ -0,0 +1,34 @@
+package generic.test.ex2;
+
+public class Pair <T1,T2>{
+
+    private T1 t1;
+    private T2 t2;
+
+    public Pair() {
+    }
+
+    public T1 getFirst() {
+        return t1;
+    }
+
+    public void setFirst(T1 t1) {
+        this.t1 = t1;
+    }
+
+    public T2 getSecond(){
+        return t2;
+    }
+
+    public void setSecond(T2 t2) {
+        this.t2 = t2;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "t1=" + t1 +
+                ", t2=" + t2 +
+                '}';
+    }
+}
