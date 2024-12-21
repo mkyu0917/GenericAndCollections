4edeb44f1f3478fb0adda9c9203f49d076cb53c6
KimMinKyu <mkyu0917@gmail.com>
2024-12-21 15:31:09 +0900

    Generic - 제네릭 적용(animal, cat, dog) 클래스 상속

---
+++ src/generic/Animal/Dog.java
@@ -0,0 +1,15 @@
+package generic.Animal;
+
+public class Dog extends Animal{
+
+
+    public Dog(String name, int size) {
+        super(name, size);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("멍멍");
+    }
+
+}
